2f1456fa8d2083735bc9df2dd8df96bb96b68f78
PaulinaRog <rog.paulina@gmail.com>
2023-01-06 12:17:14 +0100

    form

--- makeup_portfolio/src/views/ContactPage.jsx
+++ makeup_portfolio/src/views/ContactPage.jsx
@@ -1,14 +1,70 @@
 import React from "react";
+import { useState } from "react";
 import LeftNav from "../components/nav/LeftNav";
 
 export default function ContactPage() {
+  const [style, setStyle] = useState(null);
+  const [style2, setStyle2] = useState(null);
+  const [style3, setStyle3] = useState(null);
+
+  const handleClick = () => {
+    setStyle({ animation: "label linear 0.5s", left: "1em", top: "0.75em" });
+  };
+
+  const handleClick2 = () => {
+    setStyle2({ animation: "label linear 0.5s", left: "1em", top: "0.75em" });
+  };
+
+  const handleClick3 = () => {
+    setStyle3({ animation: "label linear 0.5s", left: "1em", top: "0.75em" });
+  };
+
   return (
     <div className="index">
       <LeftNav />
       <div className="contact">
         <div className="contact-2">
           <div className="contact-3">
-            <div className="contact-4"></div>
+            <div className="contact-4">
+              <form className="contact-form">
+                <div className="contact-input-container">
+                  <label className="contact-label" style={style ? style : null}>
+                    name
+                  </label>
+                  <input
+                    type="text"
+                    className="contact-input"
+                    onClick={handleClick}
+                  />
+                </div>
+                <div className="contact-input-container">
+                  <label
+                    className="contact-label"
+                    style={style2 ? style2 : null}
+                  >
+                    mail
+                  </label>
+                  <input
+                    type="text"
+                    className="contact-input"
+                    onClick={handleClick2}
+                  />
+                </div>
+                <div className="contact-input-container">
+                  <label
+                    className="contact-label"
+                    style={style3 ? style3 : null}
+                  >
+                    your message
+                  </label>
+                  <textarea
+                    type="text"
+                    className="contact-textarea textarea"
+                    onClick={handleClick3}
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
